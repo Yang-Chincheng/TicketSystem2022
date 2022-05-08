8ac538076081060da0eb05bdb2ba16a41e179a86
Modem514 <indigea514@gmail.com>
2022-05-08 14:03:15 +0800

    [lib] feat: time and date properties

---
+++ lib/time.cpp
@@ -0,0 +1,139 @@
+#include "time.h"
+
+
+namespace ticket {
+
+/**
+ * @brief implementaion of struct Date; 
+ */
+Date& Date::operator += (int days) {
+    day += days;
+    while(day > __day_number[month]) day -= __day_number[month++];
+    return *this;
+}
+Date& Date::operator -= (int days) {
+    day -= days;
+    while(day <= 0) day += __day_number[--month];
+    return *this;
+}
+
+Date::operator std::string() const {
+    std::string ret = "";
+    ret = char('0' + month % 10) + ret;
+    ret = char('0' + month / 10) + ret;
+    ret = ret + '-';
+    ret = ret + char('0' + day / 10);
+    ret = ret + char('0' + day % 10);
+    return ret;
+}
+
+std::ostream& operator << (std::ostream &os, const Date &date) {
+    os << date.month / 10 << date.month % 10; 
+    os << '-';
+    os << date.day / 10 << date.day % 10;
+    return os;
+}
+
+bool operator == (const Date &lhs, const Date &rhs) {
+    return lhs.month == rhs.month && lhs.day == rhs.day;
+}
+bool operator != (const Date &lhs, const Date &rhs) {
+    return lhs.month != rhs.month || lhs.day != rhs.day;
+}
+bool operator < (const Date &lhs, const Date &rhs) {
+    if(lhs.month != rhs.month) return lhs.month < rhs.month;
+    return lhs.day < rhs.day;
+}
+bool operator > (const Date &lhs, const Date &rhs) {
+    if(lhs.month != rhs.month) return lhs.month > rhs.month;
+    return lhs.day > rhs.day;
+}
+bool operator <= (const Date &lhs, const Date &rhs) {
+    if(lhs.month != rhs.month) return lhs.month < rhs.month;
+    return lhs.day <= rhs.day;
+}
+bool operator >= (const Date &lhs, const Date &rhs) {
+    if(lhs.month != rhs.month) return lhs.month > rhs.month;
+    return lhs.day >= rhs.day;
+}
+int operator - (const Date &lhs, const Date &rhs) {
+    int days = __prefix_total[lhs.month - 1] - __prefix_total[rhs.month - 1];
+    return days + lhs.day - rhs.day;
+}
+
+/**
+ * @brief implementation of struct Time;  
+ */
+
+Time::operator std::string() const {
+    std::string ret = "";
+    ret += '0' + hour / 10;
+    ret += '0' + hour % 10;
+    ret += ':';
+    ret += '0' + min / 10;
+    ret += '0' + min % 10;
+    return ret;
+}
+
+std::ostream& operator << (std::ostream& os, const Time& time) {
+    os << time.hour / 10 << time.hour % 10;
+    os << ':';
+    os << time.min / 10 << time.min % 10;
+    return os;
+}
+
+Time& Time::operator += (int mins) {
+    min += mins;
+    if(min >= 60) {
+        hour += min / 60, min %= 60;
+        if(hour >= 24) date += hour / 24, hour %= 24;
+    }
+    return *this;
+}
+Time& Time::operator -= (int mins) {
+    min -= mins;
+    if(min < 0) {
+        int tmp = (-min + 59) / 60;
+        hour -= tmp, min += 60 * tmp;
+        if(hour < 0) {
+            tmp = (-hour + 23) / 24;
+            date -= tmp, hour += 24 * tmp;
+        }
+    }
+    return *this;
+}
+
+bool operator == (const Time& lhs, const Time& rhs) {
+    return lhs.hour == rhs.hour && lhs.min == rhs.min && lhs.date == rhs.date;
+}
+bool operator != (const Time& lhs, const Time& rhs) {
+    return lhs.hour != rhs.hour || lhs.min != rhs.min || lhs.date != rhs.date;
+}
+bool operator < (const Time& lhs, const Time& rhs) {
+    if(lhs.date != rhs.date) return lhs.date < rhs.date;
+    if(lhs.hour != rhs.hour) return lhs.hour < rhs.hour;
+    return lhs.min < rhs.min;
+}
+bool operator > (const Time& lhs, const Time& rhs) {
+    if(lhs.date != rhs.date) return lhs.date > rhs.date;
+    if(lhs.hour != rhs.hour) return lhs.hour > rhs.hour;
+    return lhs.min > rhs.min;
+}
+bool operator <= (const Time& lhs, const Time& rhs) {
+    if(lhs.date != rhs.date) return lhs.date < rhs.date;
+    if(lhs.hour != rhs.hour) return lhs.hour < rhs.hour;
+    return lhs.min <= rhs.min;
+}
+bool operator >= (const Time& lhs, const Time& rhs) {
+    if(lhs.date != rhs.date) return lhs.date > rhs.date;
+    if(lhs.hour != rhs.hour) return lhs.hour > rhs.hour;
+    return lhs.min >= rhs.min;
+}
+
+int operator - (const Time& lhs, const Time& rhs) {
+    int mins = (lhs.date - rhs.date) * 1440;
+    mins += (lhs.hour - rhs.hour) * 60;
+    return mins + lhs.min - rhs.min;
+}
+
+}
---
+++ lib/time.h
@@ -0,0 +1,90 @@
+#ifndef _TICKET_SYSTEM_TIME_H_
+#define _TICKET_SYSTEM_TIME_H_
+
+#include <iostream>
+
+namespace ticket {
+
+/**
+ * @brief a date manager
+ * 
+ * records date in a common year, where there's 28 days in Feb and 365 days in total
+ * date info is formatted as mm-dd. (for example, August 17th -> 08-17)
+ * support several date calculations and type conversions
+ * 
+ */
+
+// numbers of days per month
+const int __day_number[13] = {
+    0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+};
+// total numbers of days per month in prefix order 
+const int __prefix_total[13] = {
+    0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365
+};
+
+struct Date {
+    int month, day;
+    Date() = default;
+    Date(int mn, int dy): month(mn), day(dy) {}
+    Date(const Date &o) = default;
+    ~Date() = default;
+
+    Date& operator += (int days);
+    Date& operator -= (int days);
+
+    operator std::string() const;
+
+    friend std::ostream& operator << (std::ostream &os, const Date &date);
+
+    friend bool operator == (const Date &lhs, const Date &rhs);
+    friend bool operator != (const Date &lhs, const Date &rhs);
+    friend bool operator <  (const Date &lhs, const Date &rhs);
+    friend bool operator >  (const Date &lhs, const Date &rhs);
+    friend bool operator <= (const Date &lhs, const Date &rhs);
+    friend bool operator >= (const Date &lhs, const Date &rhs);
+
+    friend int operator - (const Date &lhs, const Date &rhs);
+
+};
+
+/**
+ * @brief a time manager, with date info attached
+ * 
+ * records a point of time, instead of a duration
+ * date info is attached for time changes across the midnight
+ * time info is formatted as hh:mm, and uses a 24-hour clock (for example, ten to two in the afternoon -> 13:50)
+ * support several time calculations and type conversions
+ * when used for display or conversion, date info is omitted
+ *   
+ */
+
+struct Time {
+    Date date;
+    int hour, min;
+    
+    Time() = default;
+    Time(int hr, int mi, int mn = 1, int dy = 1): date(mn, dy), hour(hr), min(mi) {}
+    Time(const Time& o) = default;
+    ~Time() = default;
+
+    operator std::string() const;
+
+    friend std::ostream& operator << (std::ostream& os, const Time& time);
+
+    Time& operator += (int mins);
+    Time& operator -= (int mins);
+
+    friend bool operator == (const Time& lhs, const Time& rhs);
+    friend bool operator != (const Time& lhs, const Time& rhs);
+    friend bool operator <  (const Time& lhs, const Time& rhs);
+    friend bool operator >  (const Time& lhs, const Time& rhs);
+    friend bool operator <= (const Time& lhs, const Time& rhs);
+    friend bool operator >= (const Time& lhs, const Time& rhs);
+
+    friend int operator - (const Time& lhs, const Time& rhs);
+};
+
+}
+
+#endif
