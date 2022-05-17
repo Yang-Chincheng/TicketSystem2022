06a3089e9ef69c31ae0b69099764f2ff5bcf5254
Modem514 <indigea514@gmail.com>
2022-05-17 18:40:51 +0800

    feat(lib): add some incidental features

--- lib/utility.h
+++ lib/utility.h
@@ -30,6 +30,40 @@ struct pair {
     pair(const pair<_U1, _U2> &o): first(o.first), second(o.second) {}
     ~pair() = default;
 
+    template <typename _u1, typename _u2, typename _v1, typanem _v2>
+    friend bool operator < (const pair<_u1, _u2> &lhs, const pair<_v1, _v2> &rhs) {
+        if(lhs.first < rhs.first) return 1;
+        if(rhs.first < lhs.first) return 0;
+        return lhs.second < rhs.second;
+    }
+    template <typename _u1, typename _u2, typename _v1, typanem _v2>
+    friend bool operator > (const pair<_u1, _u2> &lhs, const pair<_v1, _v2> &rhs) {
+        if(lhs.first < rhs.first) return 0;
+        if(rhs.first < lhs.first) return 1;
+        return rhs.second < lhs.second;
+    }
+    template <typename _u1, typename _u2, typename _v1, typanem _v2>
+    friend bool operator <= (const pair<_u1, _u2> &lhs, const pair<_v1, _v2> &rhs) {
+        if(lhs.first < rhs.first) return 1;
+        if(rhs.first < lhs.first) return 0;
+        return lhs.second < rhs.second || lhs.second == rhs.second;
+    }
+    template <typename _u1, typename _u2, typename _v1, typanem _v2>
+    friend bool operator >= (const pair<_u1, _u2> &lhs, const pair<_v1, _v2> &rhs) {
+        if(lhs.first < rhs.first) return 0;
+        if(rhs.first < lhs.first) return 1;
+        return rhs.second < lhs.second || lhs.second == rhs.second;
+    }
+    template <typename _u1, typename _u2, typename _v1, typanem _v2>
+    friend bool operator == (const pair<_u1, _u2> &lhs, const pair<_v1, _v2> &rhs) {
+        return lhs.first == rhs.first && lhs.second == rhs.second;
+    }
+    template <typename _u1, typename _u2, typename _v1, typanem _v2>
+    friend bool operator != (const pair<_u1, _u2> &lhs, const pair<_v1, _v2> &rhs) {
+        return !(lhs.first == rhs.first && lhs.second == rhs.second);
+    }
+
+
 };
 
 /**
