a98f4b61e40341666e079ade9d9ac047f0ad9ee9
Modem514 <indigea514@gmail.com>
2022-07-15 20:49:00 +0800

    refactor(app): remove misc

--- app/server.py
+++ app/server.py
@@ -23,6 +23,9 @@ def connect_strlist(strlist):
     return ret[0: len(ret) - 1]
 
 
+user_now = ""
+
+
 @app.route("/index", methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
@@ -30,6 +33,7 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    global user_now
     if request.method == "GET":
         return render_template('login.html')
     if request.method == "POST":
@@ -42,17 +46,18 @@ def login():
         if row[1] == '-1':
             return json.dumps({"code": row[1], "data": ""})
         else:
-            user_now = user_name
-            return json.dumps({"code": row[1], "data": user_name})
+            user_now=user_name
+            return json.dumps({"code": "0", "data": user_name})
 
 
-@app.route('/user/index', methods=['GEt', 'POST'])
+@app.route('/user/index', methods=['GET', 'POST'])
 def user_index():
-    return render_template('user/index.html')
+    return render_template('user/index.html',username = user_now)
 
 
-@app.route('/user/buy-ticket', methods=['GEt', 'POST'])
+@app.route('/user/buy-ticket', methods=['GET', 'POST'])
 def user_buyticket():
+    global user_now
     if request.method == "GET":
         return render_template('user/buy-ticket.html')
     if request.method == "POST":
@@ -66,6 +71,11 @@ def user_buyticket():
         query = "[{}] buy_ticket -u {} -i {} -d {} -n {} -f {} -t {} -q {}".format(time_now, user_now, id, date, num,
                                                                                    start, end, pending)
         output = courier.buy_ticket(query)
+
+        print('[checkbox]', pending)
+        print('>>>', query)
+        print('<<<', output)
+
         first_row = output[0].split()
         if first_row[1] == '-1':
             info = {"code": first_row[1], 'data': ""}
@@ -75,15 +85,19 @@ def user_buyticket():
             info = {"code": '0', "data": first_row[1]}
             return json.dumps(info)
 
-
 @app.route('/user/query-order', methods=['GEt', 'POST'])
 def user_queryorder():
+    global user_now
     if request.method == "GET":
         return render_template('user/query-order.html')
     if request.method == "POST":
         time_now = time.strftime("%m%d%H%M%S", time.localtime())
         query = "[{}] query_order -u {}".format(time_now, user_now)
+        print(query)
         output = courier.query_order(query)
+
+        print(">>>", output)
+
         first_row = output[0].split()
         num = int(first_row[1])
         if num == -1:
@@ -93,8 +107,9 @@ def user_queryorder():
             for i in range(1, len(output) - 1):
                 row = output[i].split()
                 row_1 = {"status": row[0], "id": row[1], "from": row[2], "leave": "{} {}".format(row[3], row[4]),
-                         "to": row[6], "arrive": "{} {}".format(row[7], row[8]), "price": row[8], "ticket": row[9]}
+                         "to": row[6], "arrive": "{} {}".format(row[7], row[8]), "price": row[9], "ticket": row[10]}
                 orders.append(row_1)
+                print(row_1)
             # print(trains)
             info = {
                 "code": '0', "data": {
@@ -102,11 +117,13 @@ def user_queryorder():
                     "trains": orders
                 }
             }
+            print(">>", info)
             return json.dumps(info)
 
 
 @app.route('/user/query-train', methods=['GEt', 'POST'])
 def user_querytrain():
+    global user_now
     if request.method == "GET":
         return render_template('user/query-train.html')
     if request.method == "POST":
@@ -143,6 +160,7 @@ def user_querytrain():
 
 @app.route('/user/query-ticket', methods=['GEt', 'POST'])
 def user_queryticket():
+    global user_now
     if request.method == "GET":
         return render_template('user/query-ticket.html')
     if request.method == "POST":
@@ -174,29 +192,34 @@ def user_queryticket():
         else:
             query = "[{}] query_transfer -s {} -t {} -d {} -p {}".format(str(time_now), start, to, date, appendix)
             output = courier.query_transfer(query)
+
+            print(">>>", query)
+            print("<<<", output)
+
             first_row = output[0].split()
-            num = int(first_row[1])
-            if num == 0:
-                return {"code": '-1', 'data': ""}
+            if first_row[1]=='0':
+                return json.dumps({"code": '-1', 'data': ""})
             else:
                 trains = []
-                for i in range(0, 1):
-                    row = output[i].split()
-                    row_1 = {"id": row[0], "from": row[1], "leave": "{} {}".format(row[2], row[3]), "to": row[5],
+                row = output[0].split()
+                row_1 = {"id": row[1], "from": row[2], "leave": "{} {}".format(row[3], row[4]), "to": row[6],
+                             "arrive": "{} {}".format(row[7], row[8]), "price": row[9], "ticket": row[10]}
+                trains.append(row_1)
+                row = output[1].split()
+                row_1 = {"id": row[0], "from": row[1], "leave": "{} {}".format(row[2], row[3]), "to": row[5],
                              "arrive": "{} {}".format(row[6], row[7]), "price": row[8], "ticket": row[9]}
-                    trains.append(row_1)
+                trains.append(row_1)
                 # print(trains)
                 info = {
                     "code": '0', "data": {
-                        "num": num,
                         "trains": trains
                     }
                 }
                 return json.dumps(info)
 
-
 @app.route('/user/refund-ticket', methods=['GEt', 'POST'])
 def user_refundticket():
+    global user_now
     if request.method == "GET":
         return render_template('user/refund-ticket.html')
     if request.method == "POST":
@@ -204,12 +227,19 @@ def user_refundticket():
         num = request.form.get("num")
         query = "[{}] refund_ticket -u {} -n {}".format(time_now, user_now, num)
         output = courier.refund_ticket(query)
+
+        print(">>>", query)
+        print("<<<", output)
+
         first_row = output[0].split()
-        return {"code": first_row[1], 'data': ""}
+        info = {"code": first_row[1], 'data': ""}
+        print(">>>", info)
+        return json.dumps(info)
 
 
 @app.route('/admin/add-train', methods=['GEt', 'POST'])
 def admin_addtrain():
+    global user_now
     if request.method == "POST":
         if request.method == "POST":
             time_now = time.strftime("%m%d%H%M%S", time.localtime())
@@ -246,6 +276,7 @@ def admin_addtrain():
 
 @app.route('/admin/add-user', methods=['GEt', 'POST'])
 def admin_adduser():
+    global user_now
     if request.method == "GET":
         return render_template('admin/add-user.html')
     if request.method == "POST":
@@ -257,6 +288,7 @@ def admin_adduser():
         pri = request.form.get("pri")
         query = "[{}] add_user -c {} -u {} -p {} -n {} -m {} -g {}".format(time_now, user_now, id, password, name, mail,
                                                                            pri)
+        print(query)
         output = courier.add_user(query)
         first_row = output[0].split()
         return json.dumps({"code": first_row[1], "data": ""})
@@ -264,12 +296,25 @@ def admin_adduser():
 
 @app.route('/admin/clean', methods=['GEt', 'POST'])
 def admin_clean():
+    global user_now
     if request.method == "GET":
         return render_template('admin/clean.html')
+    if request.method == "POST":
+        time_now = time.strftime("%m%d%H%M%S", time.localtime())
+        type = request.form.get("type")
+        query = str()
+        if type == "clean":
+            query = "[{}] clean".format(time_now)
+        if type == "exit":
+            query = "[{}] exit".format(time_now)
+        output = courier.handle(query)
+        first_row = output[0].split()
+        return json.dumps({"code": first_row[1], "data": ""})
 
 
 @app.route('/admin/delete-train', methods=['GEt', 'POST'])
 def admin_deletetrain():
+    global user_now
     if request.method == "GET":
         return render_template('admin/delete-train.html')
     if request.method == "POST":
@@ -280,14 +325,14 @@ def admin_deletetrain():
         first_row = output[0].split()
         return json.dumps({"code": first_row[1], "data": ""})
 
-
 @app.route('/admin/index', methods=['GEt', 'POST'])
 def admin_index():
-    return render_template('admin/index.html')
+    return render_template('admin/index.html', username = user_now)
 
 
 @app.route('/admin/modify-profile', methods=['GEt', 'POST'])
 def admin_modifyprofile():
+    global user_now
     if request.method == "GET":
         return render_template('admin/modify-profile.html')
     if request.method == "POST":
@@ -317,15 +362,22 @@ def admin_modifyprofile():
 
 @app.route('/admin/query-user', methods=['GEt', 'POST'])
 def admin_queryuser():
+    global user_now
     if request.method == "GET":
         return render_template('admin/query-user.html')
     if request.method == "POST":
         time_now = time.strftime("%m%d%H%M%S", time.localtime())
         id = request.form.get("id")
         query = "[{}] query_profile -c {} -u {}".format(time_now, user_now, id)
-
         output = courier.modify_profile(query)
+
+        print('>>>', query)
+        print('<<<', output)
+
         first_row = output[0].split()
+
+        print('fr', output[0], first_row)
+
         if first_row[1] == '-1':
             return json.dumps({"code": first_row[1], "data": ""})
         else:
@@ -335,6 +387,7 @@ def admin_queryuser():
 
 @app.route('/admin/release-train', methods=['GEt', 'POST'])
 def admin_releasetrain():
+    global user_now
     if request.method == "GET":
         return render_template('admin/release-train.html')
     if request.method == "POST":
@@ -348,6 +401,7 @@ def admin_releasetrain():
 
 @app.route('/admin/rollback', methods=['GEt', 'POST'])
 def admin_rollback():
+    global user_now
     if request.method == "GET":
         return render_template('admin/rollback.html')
     if request.method == "POST":
@@ -362,10 +416,14 @@ def admin_rollback():
 
 @app.route('/')
 def notfound():
-    return '404 NOT Found'
+    return '223423 404 NOT Found'
+
 
 if __name__ == '__main__':
-    global user_now
+    query = '[0] add_user -c _ -u __Schale__ -p DECAGRAMMATON -n sensei -m a@b -g 20'
+    courier.add_user(query)
+    # query = '[0] login -u __Schale__ -p DECAGRAMMATON'
+    # courier.login(query)
     app.run(host='0.0.0.0',
             port=7777,
             # debug=true
--- app/test.py
+++
@@ -1,4 +0,0 @@
-
-str = 123
-
-print(f'{str}asds')
